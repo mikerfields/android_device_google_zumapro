16532ec8ac91159ebabfd1c3579fd7ab57530888
Mike Fields <mikefields4214@gmail.com>
2025-04-12 16:47:58 -0400

    Lineage => Infinity

--- infinity_common.mk
+++ infinity_common.mk
@@ -5,7 +5,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/zumapro/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += device/google/zumapro/overlay-infinity
 
 # AiAi Config
 PRODUCT_COPY_FILES += \
