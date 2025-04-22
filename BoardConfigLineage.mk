4b85e23dad2aa7aa0fbe6b126c205b8d8aad8d4d
Michael Fields <mikefields4214@gmail.com>
2025-04-21 22:08:47 -0400

    Bringup for BlissROMs

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -10,7 +10,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_LIMIT := true
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/bliss/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
     device/google/zumapro/android.hardware.security.rkp-service.citadel.xml \
     device/google/zumapro/manifest_radio_ds.xml
@@ -38,7 +38,7 @@ AB_OTA_PARTITIONS += \
     vbmeta_vendor
 
 # Reserve space for gapps install
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/bliss/config/BoardConfigReservedSize.mk
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
